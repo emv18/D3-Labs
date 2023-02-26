1b1af3f9549d98c7e33942b58a9b983df6741a10
emv18 <0226265@up.edu.mx>
2023-02-26 17:01:14 -0600

    Data Binding

--- Data Binding/js/main.js
+++ Data Binding/js/main.js
@@ -1,1 +1,16 @@
 var data = [25, 20, 15, 10, 5];
+
+var svg= d3.select("#chart-area").append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+
+var rects= svg.selectAll("rect")
+    .data(data);
+
+rects.enter()
+    .append("rect")
+        .attr("x", (d, i)=>{ return (i * 50); })
+        .attr("y", (d)=>{ return (-1) * (d-400); })
+        .attr("width", 40)
+        .attr("height", (d)=>{ return d; })
+        .attr("fill", "blue");
