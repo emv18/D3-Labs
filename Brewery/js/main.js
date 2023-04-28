7063ef7ee9b13f3212e837aba66f8885551db6f3
emv18 <0226265@up.edu.mx>
2023-04-27 22:07:14 -0600

    Project 1 Brewery v2

--- Brewery/js/main.js
+++ Brewery/js/main.js
@@ -1,4 +1,4 @@
-d3.json("data/revenue.json").then((data)=> {
+d3.json("data/revenues.json").then((data)=> {
     var margin = {top: 10, right: 10, bottom: 100, left:100};
     var width= 600;
     var height= 400;
@@ -6,8 +6,8 @@ d3.json("data/revenue.json").then((data)=> {
     var color = 200;
     var p_inner= 0.3;
     var p_outer= 0.3;
-    var heights = [];
-    var names = []; 
+    var revenues = [];
+    var months = []; 
     var g = d3.select("body")
         .append("svg")
         .attr("width", width + margin.right + margin.left)
@@ -16,30 +16,30 @@ d3.json("data/revenue.json").then((data)=> {
         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
     data.forEach((d)=>{
-        d.height = +d.height;
+        d.revenue = +d.revenue;
         console.log(d);
-        heights.push(d.height);
-        names.push(d.name); 
+        revenues.push(d.revenue);
+        months.push(d.month); 
     });
 
     var x = d3.scaleBand()
-        .domain(names) 
+        .domain(months) 
         .range([0, width])
         .paddingInner(p_inner)
         .paddingOuter(p_outer);
 
     var y = d3.scaleLinear()
-        .domain([0, d3.max(heights)])
+        .domain([0, d3.max(revenues)])
         .range([height, 0]); 
 
     var rects = g.selectAll("rect")
         .data(data);
     rects.enter()
         .append("rect")
-        .attr("x", (d)=>{ return x(d.name); })
-        .attr("y", (d)=>{ return y(d.height); })
+        .attr("x", (d)=>{ return x(d.month); })
+        .attr("y", (d)=>{ return y(d.revenue); })
         .attr("width", fig_width)
-        .attr("height", (d)=>{ return height - y(d.height); }) // adjust height to match y-scale
+        .attr("height", (d)=>{ return height - y(d.revenue); }) // adjust height to match y-scale
         .attr("fill", 'yellow');
 
     var bottomAxis = d3.axisBottom(x).ticks(5); 
@@ -52,7 +52,7 @@ d3.json("data/revenue.json").then((data)=> {
         .attr('transform', 'rotate(-20)');
 
     var leftAxis = d3.axisLeft(y)
-        .tickFormat((d) => d + ' m')
+        .tickFormat((d) => '$' + d)
         .ticks(5);
     g.append('g').attr('class', 'left axis').call(leftAxis);
 
